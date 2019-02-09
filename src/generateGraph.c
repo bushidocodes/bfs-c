15e06ead957f5a11ab6ba7709acc8db1b2038c5c
Sean McBride <spmcbride1201@gmail.com>
2019-02-09 13:43:33 -0500

    docs: move in-line comments to correct locations

--- src/generateGraph.c
+++ src/generateGraph.c
@@ -45,33 +45,29 @@ int main(int argc, char *argv[])
 
     newHeader = malloc(sizeof(header));
     newEdgerecord = malloc(sizeof(edgerecord));
+    // Open a File
     fp = fopen(filePath, "w");
 
+    // Calculate number of vertices: 2^(scale int)
     newHeader->numVertices = pow(2.0, scale);
+    // Calculate number of edges: #nodes * edgefactor
     newHeader->numEdges = newHeader->numVertices * edgeFactor;
 
     printf("Scale of %d yields %lu vertices\n", scale, newHeader->numVertices);
     printf("Edge Factor of %d yields %lu edges\n", edgeFactor, newHeader->numEdges);
-    // Writing Edge List Header to Text file
+    // Writing Edge List Header to Text file (#nodes #edges)
     fprintf(fp, "%lu %lu\n", newHeader->numVertices, newHeader->numEdges);
 
-    // Write the header record to a binary file
     srand(time(0));
     for (uint64_t i = 1; i <= newHeader->numEdges; i++)
     {
+        // For Loop to generate a random connection between two nodes until complete
         newEdgerecord->source = (rand_uint64() % newHeader->numVertices) + 1;
         newEdgerecord->destination = (rand_uint64() % newHeader->numVertices) + 1;
         // Write the edge record to a text file
         fprintf(fp, "%lu %lu\n", newEdgerecord->source, newEdgerecord->destination);
     }
 
-    // Calculate number of nodes: 2^(scale int)
-    // Calculate number of edges: #nodes * edgefactor
-    // Open a File
-    // Write the first line (#nodes #edges)
-    // For Loop to generate a random connection between two nodes until complete
-    // Write the lines out at a time to STDOUT
-    // This allows us to pipe either to a file or another process
     fclose(fp);
     free(newHeader);
     free(newEdgerecord);
